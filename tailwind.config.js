ec82d0f802d70db9174d6a5b236597b421a8bb61
yoonsoo0905 <sqsp6277@gmail.com>
2025-11-10 21:49:01 +0900

    로그인 및 회원가입 UI 최종 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/**/*.{js,jsx,ts,tsx}", // 우리가 만든 pages 폴더도 스캔하도록 추가!
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
