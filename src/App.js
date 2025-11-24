3b710b09e49403a6f15f78c05821043d104ea278
김민서 <min2@gimminseoui-MacBookAir.local>
2025-11-24 19:09:09 +0900

    문제 푸는 화면 UI / API 연결 중

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 import QuestionsPage from "./pages/Questions/QuestionsPage";
 
 function App() {
@@ -6,16 +8,17 @@ function App() {
     <div className="min-h-screen w-full bg-gradient-to-br from-purple-50 via-blue-50 to-pink-50">
       <BrowserRouter>
         <Routes>
+          {/* 기본 진입 시 로그인 페이지 */}
+          <Route path="/" element={<LoginPage />} />
+
+          {/* 로그인 */}
+          <Route path="/login" element={<LoginPage />} />
+
+          {/* 회원가입 */}
+          <Route path="/signup" element={<SignupPage />} />
+
+          {/* 문제 페이지 */}
           <Route path="/questions" element={<QuestionsPage />} />
-          <Route
-            path="/"
-            element={
-              <div className="p-10 text-center text-2xl">
-                CodeMon Master 시작 화면입니다. <br />
-                /questions 로 이동하세요.
-              </div>
-            }
-          />
         </Routes>
       </BrowserRouter>
     </div>
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://52.78.157.84:8080", 
-  // ↑ 여기 백엔드 팀원이 만든 서버 주소로 바꿔야 함
+  
 });
 
 export default api;
